065d5777031e87d8a5de7b08bbd17c852785f86d
abdul mukti <abdulmukti40390@gmail.com>
2025-07-24 06:45:13 +0700

    hapus input search in navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,11 +8,7 @@
                 </div>
                 
                 <div class="flex items-center space-x-6">
-                    <!-- Search -->
-                    <div class="hidden md:block relative">
-                        <input type="text" placeholder="Search..." class="futuristic-input w-64 px-4 py-2 rounded-xl pl-10">
-                        <i class="fas fa-search absolute left-3 top-3 text-gray-400"></i>
-                    </div>
+                
                     
                     
                     <!-- Profile Dropdown -->
